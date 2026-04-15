8a36fbaf14a3d29b6a780960acf977219bce764a
Nestil Admin <admin@nestil.in>
2026-04-16 01:03:39 +0530

    fix: import useCloudinaryUpload in property form

--- src/components/post-property-form.tsx
+++ src/components/post-property-form.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import { useEffect, useState, useMemo } from 'react';
+import { useCloudinaryUpload } from '@/hooks/use-cloudinary-upload';
 import { useForm, useWatch, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
